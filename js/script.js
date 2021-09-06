3b11904e5d0fc30ec42ec79fe58ba0e89897a11d
Danil <oksik193@gmail.com>
2021-09-06 18:59:10 +0600

    Loops and branches

--- js/script.js
+++ js/script.js
@@ -1,6 +1,10 @@
 "use strict"; //отключение всего устаревшего
 
-const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?");
+let numberOfFilms;
+// first
+while (numberOfFilms == 0 || numberOfFilms == undefined) {    
+    numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?");
+}
 
 const personalMovieDB = {
     count: numberOfFilms,
@@ -9,11 +13,29 @@ const personalMovieDB = {
     genres: [],
     private: false
 };
-
+/*
 const firstMovie = prompt("Один из просмотренных фильмов?");
 personalMovieDB.movies[firstMovie] = +prompt("На сколько оцените его?");
 const secondMovie = prompt("Один из просмотренных фильмов?");
 personalMovieDB.movies[secondMovie] = +prompt("На сколько оцените его?");
 
 console.log(personalMovieDB);
+*/
+//second
 
+for(let i = 0; i < 2; i++) {
+    const movieName = prompt("Один из просмотренных фильмов?");
+    personalMovieDB.movies[movieName] = +prompt("На сколько оцените его?");
+}
+
+console.log(personalMovieDB);
+
+if (personalMovieDB.count < 10) {
+    console.log('Просмотрено довольно мало фильмов');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log('Вы классический зритель');
+} else if (personalMovieDB.count >= 30) {
+    console.log('Вы киноман');
+} else {
+    console.log('error');
+}
