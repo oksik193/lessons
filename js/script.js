c92695862c522c783a023bcea372c1209d4d0ad3
Danil <oksik193@gmail.com>
2021-09-11 13:30:48 +0600

    first work with page

--- js/script.js
+++ js/script.js
@@ -1,88 +1,26 @@
-"use strict"; //отключение всего устаревшего
+/* Задания на урок:
 
-let numberOfFilms;
+1) Удалить все рекламные блоки со страницы (правая часть сайта)
 
-const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    private: false,
+2) Изменить жанр фильма, поменять "комедия" на "драма"
 
-    detectPersonalLevel: function (){
-        if(this.count < 10){
-            console.log('Просмотрено довольно мало фильмов', '');
-        } else if(this.count >= 10 && this.count < 30){
-            console.log('Вы классический зритель', '');
-        } else if(this.count >= 30){
-            console.log('Вы киноман', '');
-        } else {
-            console.log('error');
-        }
-    },
+3) Изменить задний фон постера с фильмом на изображение "bg.jpg". Оно лежит в папке img.
+Реализовать только при помощи JS
 
-    showMyDB: function () {
-        if (!this.private){
-            console.log(this);
-        }
-    },
+4) Список фильмов на странице сформировать на основании данных из этого JS файла.
+Отсортировать их по алфавиту 
 
-    writeYourGenres: function(){
-        for(let i = 1; i <= 3; i++){
-            const genre = prompt(`Ваш любимый жанр под номером ${i}`);
-            if(genre != null && genre != ''){
-            personalMovieDB.genres[i-1] = genre;
-            } else {
-                i--;
-            }
-            if(i == 3) {
-                this.genres.forEach(function(element, i, arr) {
-                    console.log(`Любимы жанр под номером ${i+1} это ${element}`);
-                });
-            }
-        }
-    },
+5) Добавить нумерацию выведенных фильмов */
 
-    rememberMyFilms: function() {
-        for(let i = 0; i < numberOfFilms; i++){
-            const a = prompt('Один из последних просмотренных фильмов?', ''),
-                  b = +prompt('На сколько оцените его?', '');
-            
-            if(a != null && b != null && a != '' && b != '' && a.length < 50){
-                personalMovieDB.movies[a] = b;
-                console.log('Done');
-            } else {
-                console.log('error');
-                i--;
-            }
-        }
-    },
+'use strict';
 
-    start: function() {
-        numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", '');
-        while(numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
-            numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", '');
-        }
-        this.count = numberOfFilms;
-    },
-
-    toggleVisibleMyDB: function(){
-        if(this.private == true){
-            this.private = false;
-            console.log(`private now false`);
-        } else if (this.private == false) {
-            this.private = true;
-            console.log(`private now true`);
-        } else {
-            console.log('error');
-        }
-    },
+const movieDB = {
+    movies: [
+        "Логан",
+        "Лига справедливости",
+        "Ла-ла лэнд",
+        "Одержимость",
+        "Скотт Пилигрим против..."
+    ]
 };
 
-personalMovieDB.start();
-personalMovieDB.rememberMyFilms();
-personalMovieDB.writeYourGenres();
-personalMovieDB.detectPersonalLevel();
-personalMovieDB.toggleVisibleMyDB();
-
-console.log(personalMovieDB);
